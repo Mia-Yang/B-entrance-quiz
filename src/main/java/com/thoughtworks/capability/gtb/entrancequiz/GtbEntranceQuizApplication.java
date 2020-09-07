f3fda031cf73bbf4be3501cde0816a3aba06b003
Yang Siyu <siyuyang1995@gmail.com>
2020-09-07 16:28:36 +0800

    [yang siyu]feat: should get name list

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/GtbEntranceQuizApplication.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/GtbEntranceQuizApplication.java
@@ -1,8 +1,15 @@
 package com.thoughtworks.capability.gtb.entrancequiz;
 
+import domain.Person;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 @SpringBootApplication
 public class GtbEntranceQuizApplication {
 
@@ -10,4 +17,16 @@ public class GtbEntranceQuizApplication {
 		SpringApplication.run(GtbEntranceQuizApplication.class, args);
 	}
 
+	private List<Person> nameList= initList();
+
+	private List<Person> initList() {
+		List<Person> nameList= new ArrayList<>();
+		nameList.add(new Person(1, "杨思雨"));
+		nameList.add(new Person(2, "黄云洁"));
+		nameList.add(new Person(3, "贠晨曦"));
+		return nameList;
+	}
+
+    @GetMapping("/nameList")
+	public List<Person> getNameList() {return nameList;}
 }
--- src/test/java/com/thoughtworks/capability/gtb/entrancequiz/GtbEntranceQuizApplicationTests.java
+++ src/test/java/com/thoughtworks/capability/gtb/entrancequiz/GtbEntranceQuizApplicationTests.java
@@ -1,13 +1,37 @@
 package com.thoughtworks.capability.gtb.entrancequiz;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class GtbEntranceQuizApplicationTests {
+	@Autowired
+	MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void should_get_name_list() throws Exception {
+		mockMvc.perform(get("/nameList"))
+				.andExpect(jsonPath("$", hasSize(3)))
+				.andExpect(jsonPath("$[0].id",is(1)))
+				.andExpect(jsonPath("$[0].name",is("杨思雨")))
+				.andExpect(jsonPath("$[1].id",is(2)))
+				.andExpect(jsonPath("$[1].name",is("黄云洁")))
+				.andExpect(jsonPath("$[2].id",is(3)))
+				.andExpect(jsonPath("$[2].name",is("贠晨曦")))
+				.andExpect(status().isOk());
+	}
 }
