3884b419b181434a1ef5acb41e8b777f19a44d80
Yang Siyu <siyuyang1995@gmail.com>
2020-09-07 21:57:42 +0800

    [yang siyu] fix: modify the test data

--- src/test/java/com/thoughtworks/capability/gtb/entrancequiz/GtbEntranceQuizApplicationTests.java
+++ src/test/java/com/thoughtworks/capability/gtb/entrancequiz/GtbEntranceQuizApplicationTests.java
@@ -29,27 +29,23 @@ class GtbEntranceQuizApplicationTests {
 	@Test
 	void should_get_name_list() throws Exception {
 		mockMvc.perform(get("/list"))
-				.andExpect(jsonPath("$", hasSize(3)))
+				.andExpect(jsonPath("$", hasSize(37)))
 				.andExpect(jsonPath("$[0].id",is(1)))
-				.andExpect(jsonPath("$[0].name",is("杨思雨")))
-				.andExpect(jsonPath("$[1].id",is(2)))
-				.andExpect(jsonPath("$[1].name",is("姚明")))
-				.andExpect(jsonPath("$[2].id",is(3)))
-				.andExpect(jsonPath("$[2].name",is("贠晨曦")))
+				.andExpect(jsonPath("$[0].name",is("廖浚斌")))
 				.andExpect(status().isOk());
 	}
 
 	@Test
 	void should_add_new_person() throws Exception {
-		Person person = new Person(4, "哈哈");
+		Person person = new Person(38, "杨恒宇");
 		ObjectMapper objectMapper = new ObjectMapper();
 		String jsonString = objectMapper.writeValueAsString(person);
 		mockMvc.perform(post("/list").content(jsonString).contentType(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk());
 		mockMvc.perform(get("/list"))
-				.andExpect(jsonPath("$", hasSize(4)))
-				.andExpect(jsonPath("$[3].id",is(4)))
-				.andExpect(jsonPath("$[3].name",is("哈哈")))
+				.andExpect(jsonPath("$", hasSize(38)))
+				.andExpect(jsonPath("$[37].id",is(38)))
+				.andExpect(jsonPath("$[37].name",is("杨恒宇")))
 				.andExpect(status().isOk());
 	}
 }
