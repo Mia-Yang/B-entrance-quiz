60ca69e834ec95e2be9d42d633822f01b3fd31a3
Yang Siyu <siyuyang1995@gmail.com>
2020-09-07 16:41:20 +0800

    [yang siyu]feat: should add new person into list

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/GtbEntranceQuizApplication.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/GtbEntranceQuizApplication.java
@@ -4,6 +4,8 @@ import domain.Person;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -22,11 +24,16 @@ public class GtbEntranceQuizApplication {
 	private List<Person> initList() {
 		List<Person> nameList= new ArrayList<>();
 		nameList.add(new Person(1, "杨思雨"));
-		nameList.add(new Person(2, "黄云洁"));
+		nameList.add(new Person(2, "姚明"));
 		nameList.add(new Person(3, "贠晨曦"));
 		return nameList;
 	}
 
-    @GetMapping("/nameList")
+    @GetMapping("/group/nameList")
 	public List<Person> getNameList() {return nameList;}
+
+	@PostMapping("/group/person")
+	public void addPerson(@RequestBody Person person) {
+		nameList.add(person);
+	}
 }
--- src/test/java/com/thoughtworks/capability/gtb/entrancequiz/GtbEntranceQuizApplicationTests.java
+++ src/test/java/com/thoughtworks/capability/gtb/entrancequiz/GtbEntranceQuizApplicationTests.java
@@ -1,14 +1,18 @@
 package com.thoughtworks.capability.gtb.entrancequiz;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import domain.Person;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -24,14 +28,29 @@ class GtbEntranceQuizApplicationTests {
 
 	@Test
 	void should_get_name_list() throws Exception {
-		mockMvc.perform(get("/nameList"))
+		mockMvc.perform(get("/group/nameList"))
 				.andExpect(jsonPath("$", hasSize(3)))
 				.andExpect(jsonPath("$[0].id",is(1)))
 				.andExpect(jsonPath("$[0].name",is("杨思雨")))
 				.andExpect(jsonPath("$[1].id",is(2)))
-				.andExpect(jsonPath("$[1].name",is("黄云洁")))
+				.andExpect(jsonPath("$[1].name",is("姚明")))
 				.andExpect(jsonPath("$[2].id",is(3)))
 				.andExpect(jsonPath("$[2].name",is("贠晨曦")))
 				.andExpect(status().isOk());
+	}
+
+	@Test
+	void should_add_new_person() throws Exception {
+		Person person = new Person(4, "哈哈");
+		ObjectMapper objectMapper = new ObjectMapper();
+		String jsonString = objectMapper.writeValueAsString(person);
+		mockMvc.perform(post("/group/person").content(jsonString).contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk());
+		mockMvc.perform(get("/group/nameList"))
+				.andExpect(jsonPath("$", hasSize(4)))
+				.andExpect(jsonPath("$[3].id",is(4)))
+				.andExpect(jsonPath("$[3].name",is("哈哈")))
+				.andExpect(status().isOk());
+
 	}
 }
