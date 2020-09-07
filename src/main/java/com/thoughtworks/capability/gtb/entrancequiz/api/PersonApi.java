8cf3344eb5d2bb99cab165983e14b7b1c3f8d71b
Yang Siyu <siyuyang1995@gmail.com>
2020-09-07 20:57:15 +0800

    [yang siyu] feat: input name list and use crossOrigin

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/api/PersonApi.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/api/PersonApi.java
@@ -1,24 +1,28 @@
 package com.thoughtworks.capability.gtb.entrancequiz.api;
 
 import domain.Person;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:1234")
 public class PersonApi {
 
     private List<Person> nameList= initList();
 
     private List<Person> initList() {
         List<Person> nameList= new ArrayList<>();
-        nameList.add(new Person(1, "杨思雨"));
-        nameList.add(new Person(2, "姚明"));
-        nameList.add(new Person(3, "贠晨曦"));
+        List<String> names = Arrays.asList(
+                "廖浚斌", "沈乐棋", "徐慧慧", "陈思聪", "王江林", "王登宇", "杨思雨", "江雨舟",
+                "廖燊", "胡晓", "但杰", "盖迈达","肖美琦", "黄云洁", "齐瑾浩", "刘亮亮","肖逸凡",
+                "王作文", "郭瑞凌", "李明豪", "党泽", "肖伊佐", "贠晨曦", "李康宁", "童世祁", "马庆", "商婕",
+                "余榕", "谌哲", "董翔锐", "陈泰宇", "赵允齐", "张柯", "廖文强", "何力", "岳港", "凌凤仪");
+        for (int i = 0; i < names.size(); i++ ) {
+            nameList.add(new Person(i+1, names.get(i)));
+        }
         return nameList;
     }
 
